6c12f6235cbbef6cb5ab22b52309fbbbfc9869ef
Fadiran <Oluwatoyin.Fadiran.Contractor@pepsico.com>
2026-07-27 16:15:03 -0500

    working with different configs

--- src/config.py
+++ src/config.py
@@ -17,6 +17,24 @@ RAW_IMAGE_DIR = Path(
 
 
 # Future outputs from detection stage
-CROPPED_IMAGE_DIR = PROJECT_ROOT / "data" / "cropped"
+CROPPED_IMAGE_DIR = PROJECT_ROOT / "MD_LicensePlate_Fadiran_ITAI1378" / "data" / "cropped"
 
-DETECTION_METADATA_DIR = PROJECT_ROOT / "data" / "detections"
+DETECTION_METADATA_DIR = PROJECT_ROOT / "MD_LicensePlate_Fadiran_ITAI1378" / "data" / "detections"
+
+
+#####################################################
+
+
+PROJECT_ROOT = Path("/content/drive/MyDrive/LPR_Project")
+
+DATA_DIR = PROJECT_ROOT / "MD_LicensePlate_Fadiran_ITAI1378" / "data"
+
+RAW_DIR = DATA_DIR / "raw"
+
+CROPPED_DIR = DATA_DIR / "cropped"
+
+MODELS_DIR = PROJECT_ROOT / "models"
+
+BEST_MODEL = MODELS_DIR / "best.pt"
+
+DATABASE = PROJECT_ROOT / "database" / "lpr.db"
